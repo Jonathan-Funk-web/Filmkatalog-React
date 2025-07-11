c7b610b3bdbe86b73d4d2999130a18081ab5faa5
Jonethan Torp <Jonathan.Torp@Funkweb.org>
2025-07-11 10:18:24 +0200

    Added basic movie detail info

--- filmkatalog/src/components/MovieDetails.js
+++ filmkatalog/src/components/MovieDetails.js
@@ -1,7 +1,18 @@
 export default function MovieDetails({ movie }) {
+  if (!movie) return <section className="movie-details">Select a movie to see details</section>;
+
+  const releaseYear = new Date(movie.release_date).getFullYear();
+  console.log()
+
   return (
     <section className="movie-details">
-      <h2>{movie?.title || "No movie selected"}</h2>
+      <h2 className="movie-details-title">{movie ? `${movie.title}` : "No movie selected"}</h2>
+      <h3 className="movie-details-year">{movie ? `(${releaseYear})` : ""}</h3>
+      
+      <hr></hr>
+
+      <p className="movie-details-description">{movie ? `${movie.overview}` : "No description"}</p>
     </section>
+
   );
 }
