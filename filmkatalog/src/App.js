118bcfe83db78b46e04433f356d6339c3b0dd468
Jonethan Torp <Jonathan.Torp@Funkweb.org>
2025-08-19 10:20:13 +0200

    Most of the api stuff is now "solved" now i just have to actualy fill it out

--- filmkatalog/src/App.js
+++ filmkatalog/src/App.js
@@ -5,20 +5,22 @@ import MovieList from './components/MovieList.js'
 import MovieDetails from './components/MovieDetails'
 import { use, useState } from 'react';
 import Poster from './components/Poster.js'
+import { fetchMovie } from './api/tmdb.js';
 
 function App() {
   const [movies, setMovies] = useState(MovieData) 
   const [selectedMovie, setSelectedMovie] = useState(movies[0]) // The 0 is just to test using the first movie, later it will be chosen from the list
 
-  const handleSearchClick = () => {
-    console.log("Search button clicked!"); // Placeholder will add stuff after, this is just here so I can actualy see that the react application gets updated
+  const handleSearchClick = (id) => {
+    let test = fetchMovie(id)
+    console.log("Search button clicked!", test); // Placeholder will add stuff after, this is just here so I can actualy see that the react application gets updated
   };
   
   return (
     <main className="app-container">
       <aside className="left-column">
         <MovieList movies={movies} onMovieSelect={setSelectedMovie} selectedMovie={selectedMovie} />
-        <button onClick={handleSearchClick}>Search for More</button>
+        <button onClick={() => handleSearchClick(123)}>Test stuff</button>
       </aside>
 
       <section className="middle-column">
--- filmkatalog/src/api/tmdb.js
+++ filmkatalog/src/api/tmdb.js
@@ -2,19 +2,7 @@ const tmdbkey = process.env.REACT_APP_TMDB_KEY;
 const omdbkey = process.env.REACT_APP_OMDB_KEY;
 const tmdb_base_URL = "https://api.themoviedb.org/3/"
 
-// export async function searchMovies(query) {
-//     const res = await fetch(`${tmdb_base_URL}/search/movie?api_key=${tmdbkey}&query=${query}`);
-//     if (!res.ok) throw new Error("Failed to fetch movies");
-//     return res.json();
-// }
-
-// export async function getMovieDetails(id) {
-//     const res = await fetch(`${tmdb_base_URL}/movie/${id}?api_key=${tmdbkey}`);
-//     if (!res.ok) throw new Error("Failed to fetch movie details");
-//     return res.json();
-// }
-
-export async function fetchPost(query) {
+export async function fetchMovie(query) {
     try {
         let response = await fetch(`${tmdb_base_URL}/search/movie?api_key=${tmdbkey}&query=${query}`);
         let data = await response.json();
